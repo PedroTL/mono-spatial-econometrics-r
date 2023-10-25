ff682854e0fa4f7757a6c75dac7d96963490a787
Pedro Tricoli <pedrottlucas@gmail.com>
2023-10-24 23:47:04 -0300

    a

--- fit-mono-final.R
+++ fit-mono-final.R
@@ -1,3 +1,6 @@
+pacman::p_load(dplyr, openxlsx, geobr, tmap, spdep, janitor, spatialreg, lme4, plm, tmap, 
+               DCluster, sp, raster, tidyr, install = TRUE)
+
 #### 0. Abrindo Banco de Dados, Homicidio (Contagem 2010) e variáveis independentes ####
 bd_final_ind_dep_count <- read.xlsx("C:\\Users\\pedro\\Documents\\GitHub\\mono-spatial-econometrics-r\\Banco Dados Simples\\bd_final_ind_dep_count_2_final.xlsx")
 bd_final_ind_dep_count <- read.xlsx("C:\\Users\\pedro\\Documents\\GitHub\\mono-spatial-econometrics-r\\Banco Dados Simples\\bd_final_ind_dep_count_2.xlsx")
@@ -55,7 +58,7 @@ bakcwards <- bd_final_ind_dep_count2 |>
 #### 2. Verificando variáveis com melhor ajuste - stepwise ####
 
 ##### 2.1 Modelo apenas com intercepto (Utilizando variável taxa homicidio suavizada) #####
-intercept_only <- lm(homicidio_rate_EBSL_2010 ~ 1, data = bd_final_ind_dep_count2)
+intercept_only <- lm(homicidio_rate_EBSL_2010 ~ 1, data = bakcwards)
 
 ##### 2.2 Adicionando as possíveis variáveis independentes #####
 independent <- c("gini_scaled_2010",
@@ -82,19 +85,19 @@ independent <- c("gini_scaled_2010",
                 ) 
 
 ##### 2.3 Criando novo banco de dados apenas com variáveis selecionadas e removendo NAs #####
-bakcwards <- bd_final_ind_dep_count2 |>
+bakcward <- bakcwards |>
   dplyr::select(homicidio_rate_EBSL_2010, independent) |>
   drop_na()
 
 ##### 2.4 Definindo modelo com todas as variáveis independentes #####
-model <- lm(homicidio_rate_EBSL_2010 ~ ., data = bakcwards)
+model <- lm(homicidio_rate_EBSL_2010 ~ ., data = bakcward)
 
 ##### 2.5 Performando modelo de regressão stepwise #####
-backward <- stats::step(model, direction = 'backward', scope = formula(model), trace = 0)
+backward_m <- stats::step(model, direction = 'backward', scope = formula(model), trace = 0)
 
 ##### 2.6 Vendo resultado da regressão stepwise #####
-backward$anova
-backward$coefficients
+backward_m$anova
+backward_m$coefficients
 
 ##### 2.7 Rodando Modelo 1 #####
 backwards_model <- lm(homicidio_rate_EBSL_2010 ~ 
@@ -103,30 +106,15 @@ backwards_model <- lm(homicidio_rate_EBSL_2010 ~
                         log_percent_pop_extremamente_pobre_2010 +
                         theil_scaled_2010 +
                         percent_desocupacao_18_mais_2010 +
-                        idhm_2010 +
+                        #idhm_2010 +
                         log_percent_pop_pobre_2010 +
-                        (grau_urbanizacao_2010):as.factor(metrop_binaria) +
+                        grau_urbanizacao_2010 +
                         percent_criancas_6a14_fora_escola_2010 +
-                        idhm_renda_2010,
+                        #idhm_renda_2010 +
+                        metrop_binaria,
                       bakcwards)
 
 summary(backwards_model)
-
-##### 2.8 Rodando Modelo 2 #####
-
-backwards_model2 <- lm(homicidio_rate_EBSL_2010 ~ 
-                        percent_mulher_15a17_com_um_filho_2010  +
-                        idhm_renda_2010 +
-                        theil_scaled_2010 +
-                        log_percent_pop_extremamente_pobre_2010 +
-                        percent_desocupacao_18_mais_2010 +
-                        log_percent_pop_pobre_2010 +
-                        percent_criancas_6a14_fora_escola_2010 +
-                        metrop_binaria +
-                        deleg_pm_mun_binaria_2014,
-                      bakcwards)
-
-summary(backwards_model2)
 
 #### 3. Correlação das variáveis independentes modelo 1 ####
 var <- c("pop_total_dividido_mil_2010",
@@ -134,25 +122,24 @@ var <- c("pop_total_dividido_mil_2010",
          "log_percent_pop_extremamente_pobre_2010",
          "theil_scaled_2010",
          "percent_desocupacao_18_mais_2010",
-         "idhm_2010",
+         #"idhm_2010",
          "log_percent_pop_pobre_2010",
          "grau_urbanizacao_2010",
          "metrop_binaria",
          "percent_criancas_6a14_fora_escola_2010",
-         "idhm_renda_2010") 
+         #"idhm_renda_2010",
+         "metrop_binaria") 
 
 corr <- stats::cor(bakcwards[, var], use = 'pairwise.complete.obs')
 corr
 
+car::vif(backwards_model)
+
 ##### 3.1 Salvando Banco de dados backwards_model #####
-# backwards_df <- bd_final_ind_dep_count2 |>
-  # select(Codmun7, homicidio_rate_EBSL_2010, independent)
-
-# write.xlsx(backwards_df, "C:\\Users\\pedro\\Documents\\GitHub\\mono-spatial-econometrics-r\\Banco Dados Simples\\backwards_model.xlsx")
-
 bakcwards_df <- read.xlsx("C:\\Users\\pedro\\Documents\\GitHub\\mono-spatial-econometrics-r\\Banco Dados Simples\\backwards_model.xlsx") |>
   mutate_at(vars(contains("scaled")), as.numeric) |>
-  mutate(metrop_binaria = as.factor(metrop_binaria))
+  mutate(metrop_binaria = as.factor(metrop_binaria)) |>
+  drop_na()
 
 #### 4. Visualizando variável suavizada ####
 ##### 4.1 Abrindo ShapeFile Sudeste #####
@@ -163,9 +150,9 @@ shp <- read_municipality(code_muni = "all", year = 2020) %>%
   filter(code_state %in% c(35, 31,  33, 32)) 
 
 ##### 4.2 Unindo dados do modelo stepwise #####
-shp_bakcwards_df <- shp |>
-  left_join(bakcwards_df, by = "Codmun7") |>
-  drop_na()
+shp_bakcwards_df <- bakcwards_df |>
+  left_join(shp, by = "Codmun7") |>
+  st_as_sf()
 
 ###### 4.2.1 Matriz de Vizinhança ######
 shp_bakcwards_df_sp <- as(shp_bakcwards_df, "Spatial")
@@ -175,9 +162,7 @@ nb <- poly2nb(shp_bakcwards_df_sp)
 shp_bakcwards_df_sp$numero_nb <- card(nb)
 
 ###### 4.2.3 Removendo Municipios sem Vizinhos ######
-shp_bakcwards_df_sp <- subset(shp_bakcwards_df_sp, shp_bakcwards_df_sp$numero_nb != 0)
-
-# Todos poligonos tem ao menos 1 vizinho
+shp_bakcwards_df_sp <- subset(shp_bakcwards_df_sp, shp_bakcwards_df_sp$numero_nb != 0) # Todos poligonos tem ao menos 1 vizinho
 
 ###### 4.2.4 Criando nova matriz de vizinhanca para aqueles que tem vizinho ######
 nb <- poly2nb(shp_bakcwards_df_sp)
@@ -199,7 +184,7 @@ tm_shape(shp_bakcwards_df,
   tm_fill("homicidio_rate_EBSL_2010", style = "quantile", title = "Taxa de Homícidio\nEBSL 2010", palette = "Reds") +
   tm_layout(legend.position = c("left", "BOTTOM"),
             legend.title.size = 0.8,
-            legend.text.size = 0.5)
+            legend.text.size = 0.5) 
 
 #### 4. Resíduos e previstos do Modelo ####
 ##### 4.1 Modelo #####
@@ -209,12 +194,17 @@ backwards_model <- lm(homicidio_rate_EBSL_2010 ~
                         log_percent_pop_extremamente_pobre_2010 +
                         theil_scaled_2010 +
                         percent_desocupacao_18_mais_2010 +
-                        idhm_2010 +
+                        #idhm_2010 +
                         log_percent_pop_pobre_2010 +
-                        (grau_urbanizacao_2010):as.factor(metrop_binaria) +
-                        percent_criancas_6a14_fora_escola_2010 +
-                        idhm_renda_2010,
-                      shp_bakcwards_df)
+                        grau_urbanizacao_2010 +
+                        metrop_binaria +
+                        percent_criancas_6a14_fora_escola_2010,
+                        #idhm_renda_2010
+                      shp_bakcwards_df_sp@data)
+
+summary(backwards_model)
+
+shp_bakcwards_df <- st_as_sf(shp_bakcwards_df_sp)
 
 ##### 4.1 Resíduos #####
 shp_bakcwards_df$res_backwards_model <- residuals(backwards_model)
@@ -235,9 +225,100 @@ tm_shape(shp_bakcwards_df,
   tm_layout(main.title = "Residuals", main.title.size = 0.7 ,
             legend.position = c("left", "bottom"), legend.hist.size = 0.3, legend.title.size = 0.8)
 
+#### 5. Teste de Moran Autocorrelação espacial ####
+##### 5.1 Teste Moran #####
+lm.morantest(backwards_model, listw = nb_pesos, alternative="two.sided") # P < 0.05 e ObservedMoran I 0.6 (Vizinhos apresentam correlação positiva moderada)
+
+##### 5.2 Correlograma de Moran #####
+correlograma_contiguidade <- sp.correlogram(neighbours = nb, var = shp_bakcwards_df$homicidio_rate_EBSL_2010, order = 5, method = "I")
+plot(correlograma_contiguidade)
+
+#### 6. Indicadors Locais de Associacao Espacil Lisa MAP ####
+localmoran <- localmoran(x = shp_bakcwards_df$homicidio_rate_EBSL_2010, listw = nb_pesos)
+View(localmoran)
+
+##### 6.1 Transformando Local Moran em DataFrame
+localmoran_df <- as.data.frame(localmoran)
+
+###### 6.1.1 Passando li e p value para banco de dados shp_bakcwards_df ######
+shp_bakcwards_df <- shp_bakcwards_df |>
+  mutate(moran = localmoran_df$Ii,
+         moran_p = localmoran_df$`Pr(z != E(Ii))`)
+
+tm_shape(shp_bakcwards_df) +
+  tm_fill("moran_p", style = "quantile",
+          pallette = c("red", "lightblue", "blue", "blue4"))
+
+###### 6.1.2 Diagrama de dispersão de Moran I ######
+moran.plot(x = shp_bakcwards_df$homicidio_rate_EBSL_2010, listw = nb_pesos, cex = 0.6, labels = FALSE)
+
+###### 6.1.3 Cada fator H acima da media L abaixo da media no poligono ###### 
+L1 <- factor(shp_bakcwards_df$homicidio_rate_EBSL_2010 < mean(shp_bakcwards_df$homicidio_rate_EBSL_2010), labels = c("H", "L"))
+
+###### 6.1.4 Vizinhos fator H acima da media L abaixo da media ###### 
+shp_bakcwards_df$lag_homicidio_rate_EBSL_2010 <- lag.listw(nb_pesos, var = shp_bakcwards_df$homicidio_rate_EBSL_2010)
+L2 <- factor(shp_bakcwards_df$lag_homicidio_rate_EBSL_2010 < mean(shp_bakcwards_df$lag_homicidio_rate_EBSL_2010), labels = c("H", "L"))
+
+shp_bakcwards_df$lisa <- paste(L1, L2)
+
+###### 6.1.5 Mapear apenas aqueles com moran_p < 0.05 ###### 
+lisa_map <- shp_bakcwards_df |>
+  mutate(lisa = ifelse(moran_p > 0.05, "Não Significativo", lisa))
+
+tm_shape(lisa_map,
+         bbox =  c(-53.10986, -25.35794, -38.84784, -14.23333)) + 
+  tm_fill("lisa", palette = c("red", "blue4", "lightblue", "blue", "white")) +
+  tm_borders("black")
+
+#### 7. Teste Multiplicador de Lagrange ####
+lm.LMtests(backwards_model, nb_pesos, test = c("LMerr","LMlag","RLMerr","RLMlag","SARMA"))
+
+##### 7.1 Fitting RLMlag #####
+lag_backwards_model <- lagsarlm(homicidio_rate_EBSL_2010 ~ 
+                        pop_total_dividido_mil_2010 +
+                        percent_mulher_15a17_com_um_filho_2010 +
+                        log_percent_pop_extremamente_pobre_2010 +
+                        theil_scaled_2010 +
+                        percent_desocupacao_18_mais_2010 +
+                        #idhm_2010 +
+                        log_percent_pop_pobre_2010 +
+                        grau_urbanizacao_2010 +
+                        percent_criancas_6a14_fora_escola_2010 +
+                        #idhm_renda_2010 + 
+                        metrop_binaria,
+                     data = shp_bakcwards_df_sp@data,
+                     listw = nb_pesos)
+
+summary(lag_backwards_model)
+
+# The impacts() function gives us something like OLS regression coefficients for a spatial lag model. 
+# The logic of the impacts() function is similar to the code above, it tells you the direct (local), 
+# indirect (spill-over), and total effect of a unit change in each of the predictor variables. 
+# The changes reported by impacts are the global average impact:
+impacts(lag_backwards_model, listw = nb_pesos)
+
+######################## Acabou Mono ############################################
+
+##### 7.1 Fitting RLMerror #####
+error_backwards_model <- errorsarlm(homicidio_rate_EBSL_2010 ~ 
+                                  pop_total_dividido_mil_2010 +
+                                  percent_mulher_15a17_com_um_filho_2010 +
+                                  log_percent_pop_extremamente_pobre_2010 +
+                                  theil_scaled_2010 +
+                                  percent_desocupacao_18_mais_2010 +
+                                  #idhm_2010 +
+                                  log_percent_pop_pobre_2010 +
+                                  grau_urbanizacao_2010 +
+                                  percent_criancas_6a14_fora_escola_2010,
+                                  #idhm_renda_2010,
+                                data = shp_bakcwards_df_sp@data,
+                                listw = nb_pesos)
+
+summary(lag_backwards_model)
+
 #### 5. Regressão com Pesos ####
 ##### 5.1 Calculando bandwidth adaptative kernel #####
-GWR_shp_bakcwards_df <- gwr.sel(homicidio_rate_EBSL_2010 ~
+GWR_shp_bakcwards_df <- spgwr::gwr.sel(homicidio_rate_EBSL_2010 ~
                                   pop_total_dividido_mil_2010 +
                                   percent_mulher_15a17_com_um_filho_2010 +
                                   log_percent_pop_extremamente_pobre_2010 +
@@ -249,4 +330,87 @@ GWR_shp_bakcwards_df <- gwr.sel(homicidio_rate_EBSL_2010 ~
                                   percent_criancas_6a14_fora_escola_2010 +
                                   idhm_renda_2010,
                                 data = shp_bakcwards_df, 
-                                adapt = TRUE)
+                                adapt = TRUE,
+                                coords = coords_xy)
+
+##### 5.2 Modelo GWR #####
+gwr_backwards_model <- spgwr::gwr(homicidio_rate_EBSL_2010 ~ 
+                                    pop_total_dividido_mil_2010 +
+                                    percent_mulher_15a17_com_um_filho_2010 +
+                                    log_percent_pop_extremamente_pobre_2010 +
+                                    theil_scaled_2010 +
+                                    percent_desocupacao_18_mais_2010 +
+                                    idhm_2010 +
+                                    log_percent_pop_pobre_2010 +
+                                    (grau_urbanizacao_2010):as.factor(metrop_binaria) +
+                                    percent_criancas_6a14_fora_escola_2010 +
+                                    idhm_renda_2010,
+                                  data = shp_bakcwards_df_sp@data,
+                                  adapt = GWR_shp_bakcwards_df,
+                                  hatmatrix = TRUE,
+                                  se.fit = TRUE,
+                                  coords = coords_xy) 
+
+gwr_backwards_model
+
+
+###### 5.2.1 Criando Mapa de Resultados GWR ######
+results_gwr <- as.data.frame(gwr_backwards_model$SDF)
+gwr_map <- cbind(shp_bakcwards_df, as.matrix(results_gwr))
+  
+###### 5.2.2 Distribuição espacial do Local R2 ######
+
+map_maker <- function(db, variable, maintitle, legendtitle, style) {
+  plot <- 
+    tm_shape(db, bbox = c(-53.10986, -25.35794, -38.84784, -14.23333)) +
+    tm_fill(col = variable, title = legendtitle, style = style, palette = "-RdBu", midpoint = 0) +
+    tm_borders(alpha = 0.1) +
+    tm_layout(main.title = maintitle, main.title.size = 0.7,
+              legend.position = c("left", "bottom"), legend.hist.size = 0.3, legend.title.size = 0.8)
+  
+  return(plot)
+}
+
+map_maker(gwr_map, "localR2", "Local R2", "Local R2", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "pop_total_dividido_mil_2010", "Pop. Total Dividido por Mil", "Pop. Total Dividido por Mil", "quantile")
+map_maker(gwr_map, "pop_total_dividido_mil_2010.1", "Coef. Pop. Total Dividido por Mil", "Coef. Pop. Total Dividido por Mil", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "percent_mulher_15a17_com_um_filho_2010", "% Mulheres 15 a 17 com filho", "% Mulheres 15 a 17 com filho", "quantile")
+map_maker(gwr_map, "percent_mulher_15a17_com_um_filho_2010.1", "Coef. % Mulheres 15 a 17 com filho", "Coef. % Mulheres 15 a 17 com filho", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "log_percent_pop_extremamente_pobre_2010", "Log % pop. extremamente pobre", "Log % pop. extremamente pobre", "quantile")
+map_maker(gwr_map, "log_percent_pop_extremamente_pobre_2010.1", "Coef. Log % pop. extremamente pobre", "Coef. Log % pop. extremamente pobre", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "theil_scaled_2010", "Theil Scaled", "Theil Scaled", "quantile")
+map_maker(gwr_map, "theil_scaled_2010.1", "Coef. Theil Scaled", "Theil Scaled", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "percent_desocupacao_18_mais_2010", "% Desocupação 18a +", "% Desocupação 18a ", "quantile")
+map_maker(gwr_map, "percent_desocupacao_18_mais_2010.1", "Coef. % Desocupação 18a ", "Coef. % Desocupação 18a ", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "idhm_2010", "IDHM", "IDHM", "quantile")
+map_maker(gwr_map, "idhm_2010.1", "Coef. IDHM", "Coef. IDHM", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "log_percent_pop_pobre_2010", "Log % Pop. Pobre", "Log % Pop. Pobre", "quantile")
+map_maker(gwr_map, "log_percent_pop_pobre_2010.1", "Coef. Log % Pop. Pobre", "Coef. Log % Pop. Pobre", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "grau_urbanizacao_2010", "Grau de Urbanização", "Grau de Urbanização", "quantile")
+map_maker(gwr_map, "grau_urbanizacao_2010.1", "Coef. Grau de Urbanização", "Coef. Grau de Urbanização", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "percent_criancas_6a14_fora_escola_2010", "% crianças 6a14a fora da escola", "% crianças 6a14a fora da escola", "quantile")
+map_maker(gwr_map, "percent_criancas_6a14_fora_escola_2010.1", "Coef. % crianças 6a14a fora da escola", "Coef. % crianças 6a14a fora da escola", "quantile")
+
+###### 5.2.3 Distribuição pop_total_dividido_mil_2010 e o coeficiente de pop_total_dividido_mil_2010 ######
+map_maker(gwr_map, "idhm_renda_2010", "IDHM Renda", "IDHM Renda", "quantile")
+map_maker(gwr_map, "idhm_renda_2010.1", "Coef. IDHM Renda", "Coef. IDHM Renda", "quantile")
+
+ 
